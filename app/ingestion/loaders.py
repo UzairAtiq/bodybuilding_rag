51181574c333a933bb487add9631e7df20a8d961
UzairAtiq <uzairatiq65@gmail.com>
2026-09-01 19:17:37 +0500

    refactor: accept file path as argument instead of hardcoding

--- app/ingestion/loaders.py
+++ app/ingestion/loaders.py
@@ -1,8 +1,8 @@
 #Read the file in data / raw / joe-weider-s-bodybuilding-system-joe-weider-2929.md as output as string 
 
 
-def readFileAsString () :
-  with open ("/Users/uzair/Developer/Muscle_Info_RAG/data/raw/joe-weider-s-bodybuilding-system-joe-weider-2929.md" , "r" , encoding="utf-8") as file : 
+def load_book (path : str) -> str :
+  with open (path , "r" , encoding="utf-8") as file : 
     text_content = file.read()
     return text_content
 
