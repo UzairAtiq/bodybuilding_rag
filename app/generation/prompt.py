06085ad2f86d6dd959e1a43fd68426237abda967
UzairAtiq <uzairatiq65@gmail.com>
2026-09-02 20:31:26 +0500

    fix: correct prompt.py placeholder casing, join logic, and template formatting

--- app/generation/prompt.py
+++ app/generation/prompt.py
@@ -1,14 +1,26 @@
 from langchain_core.prompts import PromptTemplate
 
-def build_prompt(query , context) :
 
-  prompt = PromptTemplate.from_template("Answer the question using only the provided context" \
-  "Question : {question}" \
-  "Context : {}")
+
+def build_prompt(query , ranked_chunks) :
+
+  print("Building Prompt")
+
+  #Recieved chunks in list join into one big string 
+  context = "\n\n".join([chunk.payload["text"] for chunk in ranked_chunks])
+
+  prompt = PromptTemplate.from_template("Answer the question using only the provided context\n" \
+  "Question : {Question} \n" \
+  "Context : {Context} \n")
 
   #Formatting the pormpt with inputs
   return prompt.format(Question = query ,
                        Context = context )
 
+
   
+
+
+
+
 
