851a5486e898b79f7e5b14198b83952475550c1a
UzairAtiq <uzairatiq65@gmail.com>
2026-09-02 20:31:26 +0500

    feat: implement pipeline.py connecting retriever, reranker, prompt, and llm

---
+++ app/pipeline.py
@@ -0,0 +1,24 @@
+from app.retrieval.retriever import retrieve
+from app.retrieval.reranker import reranker
+from app.generation.prompt import build_prompt
+from app.generation.llm import send_prompt
+
+def pipeline (query : str) :
+
+  #Sending query to retriever
+  chunks_recieved = retrieve(query) 
+
+  #Sending the query and top-k chunks to reranker for better ranking ]
+  reranked_chunks = reranker(query , chunks_recieved)
+
+  #Building the prompt
+  prompt = build_prompt(query,reranked_chunks)
+  
+  #Sending the prompt to the llmm and getting answer
+  answer = send_prompt(prompt)
+
+  return answer
+
+
+answer = pipeline(query="What are good exercises for a beginner?")
+print("LLM Answer : \n\n",answer)
